f85ebf89274590f7cf706c713f6a2538b0e965b9
Cory Pendergraft <cory.pendergraft81@gmail.com>
2023-02-17 14:38:06 -0500

    starter code from mini and deleting useles things and renameing files and folders

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,1 @@
-const router = require('express').Router();
-const courseRoutes = require('./courseRoutes');
-const studentRoutes = require('./studentRoutes');
 
-router.use('/courses', courseRoutes);
-router.use('/students', studentRoutes);
-
-module.exports = router;
